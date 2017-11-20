f5a84ad280f5382449e50420399f687d1beb737c
Jonas Keller <jonas.richardd@gmail.com>
2017-11-20 14:36:40 +0100

    add method getStation

--- src/de/misterY/Link.java
+++ src/de/misterY/Link.java
@@ -2,26 +2,34 @@ package de.misterY;
 
 public class Link {
 	private Station station;
-	
+
 	private boolean bus;
 	private boolean underground;
-	
+
 	/**
 	 * Constructs a new link.
-	 * @param targetStation The target station.
-	 * @param bus Whether the link is a bus link.
-	 * @param underground Whether the link is a underground link.
+	 * 
+	 * @param targetStation
+	 *            The target station.
+	 * @param bus
+	 *            Whether the link is a bus link.
+	 * @param underground
+	 *            Whether the link is a underground link.
 	 */
 	public Link(Station targetStation, boolean bus, boolean underground) {
 	}
-	
+
+	public Station getStation() {
+		return station;
+	}
+
 	/**
 	 * @return Whether a bus can use this link
 	 */
 	public boolean isBus() {
 		return bus;
 	}
-	
+
 	/**
 	 * @return Whether an underground can use this link
 	 */
