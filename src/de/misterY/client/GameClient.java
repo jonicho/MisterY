f321159ffeb71985c6903418fa642349ca385dba
Jonas Keller <jonas.richardd@gmail.com>
2017-11-26 19:10:22 +0100

    add basic message processing (client)

--- src/de/misterY/client/GameClient.java
+++ src/de/misterY/client/GameClient.java
@@ -1,16 +1,47 @@
 package de.misterY.client;
 
 import de.misterY.net.Client;
+import de.misterY.net.PROTOCOL;
 
 public class GameClient extends Client {
 
-	public GameClient(String pServerIP, int pServerPort) {
-		super(pServerIP, pServerPort);
+	public GameClient(String serverIP, int serverPort) {
+		super(serverIP, serverPort);
 	}
 
 	@Override
-	public void processMessage(String pMessage) {
+	public void processMessage(String message) {
+		String[] msgParts = message.split(PROTOCOL.SPLIT);
+		
+		switch (msgParts[0]) {
+		case PROTOCOL.SC.ERROR:
+			
+			break;
+		case PROTOCOL.SC.POSITION_UPDATE:
+			
+			break;
+		case PROTOCOL.SC.OK:
+			
+			break;
+		case PROTOCOL.SC.CHAT_UPDATE:
+			
+			break;
+		case PROTOCOL.SC.INFO_UPDATE:
+			
+			break;
+		case PROTOCOL.SC.MAP:
+			
+			break;
+		case PROTOCOL.SC.YOUR_TURN:
+			
+			break;
+		case PROTOCOL.SC.PLAYER_LEFT:
+			
+			break;
 
+		default:
+			break;
+		}
 	}
 
 }
