b65c95a9c1e5c062a5b544fc73c7580e5e224b51
Jonas Keller <jonas.richardd@gmail.com>
2017-12-04 21:02:47 +0100

    auto format

--- src/de/misterY/bot/ImperfectMoveGeneration.java
+++ src/de/misterY/bot/ImperfectMoveGeneration.java
@@ -2,46 +2,58 @@ package de.misterY.bot;
 
 import java.util.ArrayList;
 
+import de.misterY.Link;
 import de.misterY.Player;
 import de.misterY.Station;
-import de.misterY.Link;
 
 public class ImperfectMoveGeneration {
 	private Station currentPosition;
 	private Player LocalPlayer;
 	private ArrayList<Station> PossiblePositions = new ArrayList<Station>();
 	private Integer MoveMode = 0;
-	
-	public ImperfectMoveGeneration(Player pPlayer,Station station) {
+
+	public ImperfectMoveGeneration(Player pPlayer, Station station) {
 		currentPosition = station;
 		LocalPlayer = pPlayer;
 	}
-	
-	/** Updates our current position
-	 * @param station The new position
+
+	/**
+	 * Updates our current position
+	 * 
+	 * @param station
+	 *            The new position
 	 */
 	public void updateOwnPosition(Station station) {
 		currentPosition = station;
 	}
-	
-	/** Updates the ResolvedPositions
-	 * @param pos The Updated array
+
+	/**
+	 * Updates the ResolvedPositions
+	 * 
+	 * @param pos
+	 *            The Updated array
 	 */
 	public void updateResolvedPositions(ArrayList<Station> pos) {
 		PossiblePositions = pos;
 	}
-	
+
 	/**
-	 * Analyses the scenario we are in and tries to find the best move to execute 
+	 * Analyses the scenario we are in and tries to find the best move to execute
 	 */
 	public void doAnalysis() {
-		if (PossiblePositions.size() <= 5) MoveMode = 1; //Not a lot of possible positions -> occupie one of them
-		if (PossiblePositions.size() >= 6) MoveMode = 2; //Too many possible position -> move to the middle of all positions
-		if (PossiblePositions.size() >= 6 && LinkedTrainStation() != null) MoveMode = 3; // Many Positions and we are close to a Trainstation -> lets occupie the trainstation for now
-		//Implement more modes & Strategies
-		if (PossiblePositions.size() >= 75) MoveMode = 0; // Too many Positions to do anything logical -> just take a taxi since they are the least rare
+		if (PossiblePositions.size() <= 5)
+			MoveMode = 1; // Not a lot of possible positions -> occupie one of them
+		if (PossiblePositions.size() >= 6)
+			MoveMode = 2; // Too many possible position -> move to the middle of all positions
+		if (PossiblePositions.size() >= 6 && LinkedTrainStation() != null)
+			MoveMode = 3; // Many Positions and we are close to a Trainstation -> lets occupie the
+							// trainstation for now
+		// Implement more modes & Strategies
+		if (PossiblePositions.size() >= 75)
+			MoveMode = 0; // Too many Positions to do anything logical -> just take a taxi since they are
+							// the least rare
 	}
-	
+
 	/**
 	 * Runs whatever move was analysed to be the best in the current scenario
 	 */
@@ -55,8 +67,11 @@ public class ImperfectMoveGeneration {
 			break;
 		}
 	}
-	
-	/** Returns a train station that is directly linked to the current position or null if there isnt one
+
+	/**
+	 * Returns a train station that is directly linked to the current position or
+	 * null if there isnt one
+	 * 
 	 * @return
 	 */
 	public Station LinkedTrainStation() {
@@ -66,7 +81,8 @@ public class ImperfectMoveGeneration {
 			targets.add(l.getStation());
 		}
 		for (Station s : targets) {
-			if (s.isUnderground()) return s;
+			if (s.isUnderground())
+				return s;
 		}
 		return null;
 	}
--- src/de/misterY/bot/PositionPredicter.java
+++ src/de/misterY/bot/PositionPredicter.java
@@ -7,29 +7,44 @@ import de.misterY.Player;
 import de.misterY.Station;
 
 public class PositionPredicter {
-	public PositionPredicter() {}
-	
-	/** Predicts all Possible Movements the Specified player can make from the specified station with his tickets
-	 * @param station the station the player is on
-	 * @param player the player to predict
+	public PositionPredicter() {
+	}
+
+	/**
+	 * Predicts all Possible Movements the Specified player can make from the
+	 * specified station with his tickets
+	 * 
+	 * @param station
+	 *            the station the player is on
+	 * @param player
+	 *            the player to predict
 	 * @return All Possible Positions in an ArrayList
 	 */
 	private ArrayList<Station> PredictPositions(Station station, Player player) {
 		boolean hasBusTickets;
 		boolean hasTrainTickets;
 		boolean hasTaxiTickets;
-		if (player.getBusTickets() > 0) hasBusTickets = true;
-		else hasBusTickets = false;
-		if (player.getTaxiTickets() > 0) hasTaxiTickets = true;
-		else hasTaxiTickets = false;
-		if (player.getUndergroundTickets() > 0) hasTrainTickets = true;
-		else hasTrainTickets = false;
+		if (player.getBusTickets() > 0)
+			hasBusTickets = true;
+		else
+			hasBusTickets = false;
+		if (player.getTaxiTickets() > 0)
+			hasTaxiTickets = true;
+		else
+			hasTaxiTickets = false;
+		if (player.getUndergroundTickets() > 0)
+			hasTrainTickets = true;
+		else
+			hasTrainTickets = false;
 		ArrayList<Link> stationLinks = station.getLinks();
 		ArrayList<Link> possibleLinks = new ArrayList<Link>();
 		for (Link l : stationLinks) {
-			if (l.isBus() && hasBusTickets) possibleLinks.add(l);
-			if (l.isUnderground() && hasTrainTickets) possibleLinks.add(l);
-			if (!l.isBus() && !l.isUnderground() && hasTaxiTickets) possibleLinks.add(l);
+			if (l.isBus() && hasBusTickets)
+				possibleLinks.add(l);
+			if (l.isUnderground() && hasTrainTickets)
+				possibleLinks.add(l);
+			if (!l.isBus() && !l.isUnderground() && hasTaxiTickets)
+				possibleLinks.add(l);
 		}
 		ArrayList<Station> predictedPositions = new ArrayList<Station>();
 		for (Link k : possibleLinks) {
@@ -37,31 +52,38 @@ public class PositionPredicter {
 		}
 		return predictedPositions;
 	}
-	
-	/** Returns if for the given parameters the can be a prediction with 100% accuracy
-	 * @param station 
-	 * @param player 
+
+	/**
+	 * Returns if for the given parameters the can be a prediction with 100%
+	 * accuracy
+	 * 
+	 * @param station
+	 * @param player
 	 * @return
 	 */
 	public boolean canPredictDefinitePosition(Station station, Player player) {
-		return (PredictPositions(station,player).size()) == 1;	
+		return (PredictPositions(station, player).size()) == 1;
 	}
-	
-	/** Gets only the first position in the array 
+
+	/**
+	 * Gets only the first position in the array
+	 * 
 	 * @param station
 	 * @param player
 	 * @return
 	 */
 	public Station getDefinitePosition(Station station, Player player) {
-		return PredictPositions(station,player).get(0);
+		return PredictPositions(station, player).get(0);
 	}
-	
-	/** Gets all possible positions
+
+	/**
+	 * Gets all possible positions
+	 * 
 	 * @param station
 	 * @param player
 	 * @return
 	 */
-	public ArrayList<Station> getAllPredictions(Station station,Player player) {
-		return PredictPositions(station,player);
+	public ArrayList<Station> getAllPredictions(Station station, Player player) {
+		return PredictPositions(station, player);
 	}
 }
