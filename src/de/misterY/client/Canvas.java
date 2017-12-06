36b4f977358295b8c68031ad88feaa8edc1e132f
Jonas Keller <jonas.richardd@gmail.com>
2017-12-06 09:20:19 +0100

    implement move and zoom

--- src/de/misterY/client/Canvas.java
+++ src/de/misterY/client/Canvas.java
@@ -1,6 +1,12 @@
 package de.misterY.client;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 
 import javax.swing.JPanel;
 
@@ -13,10 +19,62 @@ public class Canvas extends JPanel {
 	private static final long serialVersionUID = 1L;
 
 	private Map map;
+	private double scale = 1;
+	private int x = 0;
+	private int y = 0;
+	private int mouseX;
+	private int mouseY;
+
+	public Canvas() {
+		addMouseWheelListener(new MouseWheelListener() {
+			private int scaleSteps = 0;
+			private double lastScale = 1;
+
+			@Override
+			public void mouseWheelMoved(MouseWheelEvent e) {
+				scaleSteps -= e.getWheelRotation();
+				if (scaleSteps > 10)
+					scaleSteps = 10;
+				if (scaleSteps < 0)
+					scaleSteps = 0;
+				scale = 1 + 0.2 * scaleSteps;
+				x = (int) (mouseX * ((lastScale / scale) - 1) + (lastScale / scale) * x);
+				y = (int) (mouseY * ((lastScale / scale) - 1) + (lastScale / scale) * y);
+				lastScale = scale;
+				repaint();
+			}
+		});
+		addMouseMotionListener(new MouseMotionListener() {
+
+			@Override
+			public void mouseMoved(MouseEvent e) {
+				mouseX = e.getX();
+				mouseY = e.getY();
+			}
+
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				x += e.getX() - mouseX;
+				y += e.getY() - mouseY;
+				mouseX = e.getX();
+				mouseY = e.getY();
+				repaint();
+			}
+		});
+	}
 
 	@Override
-	protected void paintComponent(Graphics g) {
+	protected void paintComponent(Graphics gg) {
+		Graphics2D g = (Graphics2D) gg;
 		g.clearRect(0, 0, getWidth(), getHeight());
+
+		g.scale(scale, scale);
+		g.translate(x, y);
+
+		g.setColor(new Color(200, 200, 200));
+		g.fillRect(0, 0, getWidth(), getHeight());
+
+		g.setColor(Color.BLACK);
 		drawMap(g);
 	}
 
@@ -27,7 +85,7 @@ public class Canvas extends JPanel {
 	 * @param g
 	 *            The graphics on which is drawn
 	 */
-	private void drawMap(Graphics g) {
+	private void drawMap(Graphics2D g) {
 		if (map == null) {
 			return;
 		}
@@ -47,7 +105,7 @@ public class Canvas extends JPanel {
 	 * @param station
 	 *            The station to draw the links of
 	 */
-	private void drawLinks(Graphics g, Station station) {
+	private void drawLinks(Graphics2D g, Station station) {
 		for (Link link : station.getLinks()) {
 			int x1 = station.getPos().getDrawX(getWidth());
 			int y1 = station.getPos().getDrawY(getHeight());
@@ -67,13 +125,15 @@ public class Canvas extends JPanel {
 	 * @param vec
 	 *            The direction in which the arrow will point
 	 */
-	private void drawArrow(Graphics g, Vector2D pos, Vector2D vec) {
+	private void drawArrow(Graphics2D g, Vector2D pos, Vector2D vec) {
 		vec.setLength(0.03);
 		pos.add(vec);
 		Vector2D left = vec.getClone().rotate(Math.toRadians(30)).add(pos);
 		Vector2D right = vec.getClone().rotate(Math.toRadians(-30)).add(pos);
-		g.drawLine(pos.getDrawX(getWidth()), pos.getDrawY(getHeight()), left.getDrawX(getWidth()), left.getDrawY(getHeight()));
-		g.drawLine(pos.getDrawX(getWidth()), pos.getDrawY(getHeight()), right.getDrawX(getWidth()), right.getDrawY(getHeight()));
+		g.drawLine(pos.getDrawX(getWidth()), pos.getDrawY(getHeight()), left.getDrawX(getWidth()),
+				left.getDrawY(getHeight()));
+		g.drawLine(pos.getDrawX(getWidth()), pos.getDrawY(getHeight()), right.getDrawX(getWidth()),
+				right.getDrawY(getHeight()));
 	}
 
 	/**
@@ -84,7 +144,7 @@ public class Canvas extends JPanel {
 	 * @param station
 	 *            The station which is drawn
 	 */
-	private void drawStations(Graphics g, Station station) {
+	private void drawStations(Graphics2D g, Station station) {
 		int x = station.getPos().getDrawX(getWidth());
 		int y = station.getPos().getDrawY(getHeight());
 		int size = 10;
