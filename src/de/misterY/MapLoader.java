8b384f00b5729e61883b4816c504f807daaa0646
Jonas Keller <jonas.richardd@gmail.com>
2017-12-27 12:21:29 +0100

    make map files/string smaller
    by replacing words with single letters

--- src/de/misterY/MapLoader.java
+++ src/de/misterY/MapLoader.java
@@ -61,20 +61,20 @@ public class MapLoader {
 			Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder()
 					.parse(new ByteArrayInputStream(mapString.getBytes()));
 			doc.getDocumentElement().normalize();
-			Element initialTicketsElement = (Element) doc.getElementsByTagName("initialTickets").item(0);
-			initialTickets[0] = Integer.parseInt(initialTicketsElement.getAttribute("taxi"));
-			initialTickets[1] = Integer.parseInt(initialTicketsElement.getAttribute("bus"));
-			initialTickets[2] = Integer.parseInt(initialTicketsElement.getAttribute("underground"));
-			NodeList stationElements = doc.getElementsByTagName("station");
+			Element initialTicketsElement = (Element) doc.getElementsByTagName("i").item(0);
+			initialTickets[0] = Integer.parseInt(initialTicketsElement.getAttribute("t"));
+			initialTickets[1] = Integer.parseInt(initialTicketsElement.getAttribute("b"));
+			initialTickets[2] = Integer.parseInt(initialTicketsElement.getAttribute("u"));
+			NodeList stationElements = doc.getElementsByTagName("s");
 			for (int i = 0; i < stationElements.getLength(); i++) {
 				Element stationElement = (Element) stationElements.item(i);
 
-				boolean bus = Boolean.parseBoolean(stationElement.getAttribute("bus"));
-				boolean underground = Boolean.parseBoolean(stationElement.getAttribute("underground"));
-				int id = Integer.parseInt(stationElement.getAttribute("id"));
+				boolean bus = stationElement.getAttribute("b").equalsIgnoreCase("t");
+				boolean underground = stationElement.getAttribute("u").equalsIgnoreCase("t");
+				int id = Integer.parseInt(stationElement.getAttribute("i"));
 				double x = Float.parseFloat(stationElement.getAttribute("x"));
 				double y = Float.parseFloat(stationElement.getAttribute("y"));
-				boolean start = Boolean.parseBoolean(stationElement.getAttribute("start"));
+				boolean start = stationElement.getAttribute("s").equalsIgnoreCase("t");
 
 				Station station = new Station(bus, underground, id, new Vector2D(x, y));
 				stations.add(station);
@@ -86,8 +86,8 @@ public class MapLoader {
 
 			for (int i = 0; i < stationElements.getLength(); i++) {
 				Element stationElement = (Element) stationElements.item(i);
-				NodeList linkElements = stationElement.getElementsByTagName("link");
-				int id = Integer.parseInt(stationElement.getAttribute("id"));
+				NodeList linkElements = stationElement.getElementsByTagName("l");
+				int id = Integer.parseInt(stationElement.getAttribute("i"));
 				Station station = null;
 				for (Station s : stations) {
 					if (s.getId() == id) {
@@ -98,10 +98,10 @@ public class MapLoader {
 				for (int j = 0; j < linkElements.getLength(); j++) {
 					Element linkElement = (Element) linkElements.item(j);
 
-					int targetStationId = Integer.parseInt(linkElement.getAttribute("station"));
-					boolean taxi = Boolean.parseBoolean(linkElement.getAttribute("taxi"));
-					boolean bus = Boolean.parseBoolean(linkElement.getAttribute("bus"));
-					boolean underground = Boolean.parseBoolean(linkElement.getAttribute("underground"));
+					int targetStationId = Integer.parseInt(linkElement.getAttribute("s"));
+					boolean taxi = linkElement.getAttribute("t").equalsIgnoreCase("t");
+					boolean bus = linkElement.getAttribute("b").equalsIgnoreCase("t");
+					boolean underground = linkElement.getAttribute("u").equalsIgnoreCase("t");
 
 					Station targetStation = null;
 					for (Station ts : stations) {
