4d3e6a4f2932f138c7d5c4431c0d702781322a4e
Jonas Keller <jonas.richardd@gmail.com>
2017-12-30 12:31:10 +0100

    do not use gradient when link goes in both directions to save performance

--- src/de/misterY/client/MapDrawer.java
+++ src/de/misterY/client/MapDrawer.java
@@ -54,42 +54,52 @@ public class MapDrawer {
 		for (Link link : station.getLinks()) {
 			Vector2D ort = station.getPos().getClone().subtract(link.getStation().getPos()).rotate(Math.PI / 2)
 					.setLength(0.000002 * avgSize);
+			Link oppositeLink = link.getStation().getLink(station);
 			if (ort.getX() > 0) {
 				ort.multiply(-1);
 			}
 			if (link.isTaxi()) {
-				drawLink(g, station.getPos(), link.getStation().getPos(), Color.YELLOW);
+				drawLine(g, station.getPos(), link.getStation().getPos(), Color.YELLOW,
+						oppositeLink == null || !oppositeLink.isTaxi());
 			}
 			if (link.isBus()) {
-				drawLink(g, station.getPos().getClone().add(ort), link.getStation().getPos().getClone().add(ort),
-						Color.GREEN);
+				drawLine(g, station.getPos().getClone().add(ort), link.getStation().getPos().getClone().add(ort),
+						Color.GREEN, oppositeLink == null || !oppositeLink.isBus());
 			}
 			if (link.isUnderground()) {
-				drawLink(g, station.getPos().getClone().subtract(ort),
-						link.getStation().getPos().getClone().subtract(ort), Color.RED);
+				drawLine(g, station.getPos().getClone().subtract(ort),
+						link.getStation().getPos().getClone().subtract(ort), Color.RED,
+						oppositeLink == null || !oppositeLink.isUnderground());
 			}
 		}
 	}
 
 	/**
-	 * Draws a link from v1 to v2 with the given color.<br>
-	 * Uses a gradient that has the given color at v1 and a transparent color at v2.
+	 * Draws a line from v1 to v2 with the given color.<br>
+	 * Uses a gradient that has the given color at v1 and a transparent color at v2
+	 * if the given gradient boolean is true.
 	 * 
 	 * @param g
 	 *            The graphics on which is drawn
 	 * @param v1
-	 *            The start position of the link
+	 *            The start position of the line
 	 * @param v2
-	 *            The end position of the link
+	 *            The end position of the line
 	 * @param color
 	 *            The color of the link
+	 * @param gradient
+	 *            Whether gradient should be used
 	 */
-	private void drawLink(Graphics2D g, Vector2D v1, Vector2D v2, Color color) {
+	private void drawLine(Graphics2D g, Vector2D v1, Vector2D v2, Color color, boolean gradient) {
 		int x1 = v1.getDrawX(width);
 		int y1 = v1.getDrawY(height);
 		int x2 = v2.getDrawX(width);
 		int y2 = v2.getDrawY(height);
-		g.setPaint(new GradientPaint(x1, y1, color, x2, y2, new Color(0, 0, 0, 0)));
+		if (gradient) {
+			g.setPaint(new GradientPaint(x1, y1, color, x2, y2, new Color(0, 0, 0, 0)));
+		} else {
+			g.setColor(color);
+		}
 		g.drawLine(x1, y1, x2, y2);
 	}
 
