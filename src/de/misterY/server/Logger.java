9cfdaa624b52ab25de72dd2588e5c1e92db2effa
Jonas Keller <jonas.richardd@gmail.com>
2018-03-20 20:10:14 +0100

    save log in file

--- src/de/misterY/server/Logger.java
+++ src/de/misterY/server/Logger.java
@@ -1,14 +1,35 @@
 package de.misterY.server;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Date;
 
 import de.misterY.net.PROTOCOL;
 
 public class Logger {
+	private static final File logFile;
+
+	static {
+		logFile = new File("misterY_log[" + new Date().toString() + "].txt");
+	}
+
 	public static void logMessage(String msg, User user, Session session, boolean incoming) {
-		String output = "[" + new Date().toString() + "] " + (session != null ? ("Session " + session.getId() + ": ") : "");
-		output += (incoming ? "Got message from \"" : "Sent message to \"") + user.getPlayer().getName() + "\": "
-				+ msg.replace(PROTOCOL.SPLIT, "::");
+		log((session != null ? ("Session " + session.getId() + ": ") : "")
+				+ (incoming ? "Got message from \"" : "Sent message to \"") + user.getPlayer().getName() + "\": "
+				+ msg.replace(PROTOCOL.SPLIT, "::"));
+	}
+
+	public static void log(String msg) {
+		String output = "[" + new Date().toString() + "] " + msg;
 		System.out.println(output);
+		try {
+			FileWriter fw = new FileWriter(logFile, true);
+			fw.write(output + "\n");
+			fw.flush();
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
