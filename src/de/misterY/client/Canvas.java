9571f8703dcab938c6067656196c40240f7caa76
Jonas Keller <jonas.richardd@gmail.com>
2018-04-04 17:46:09 +0200

    refactor panning and zooming

--- src/de/misterY/client/Canvas.java
+++ src/de/misterY/client/Canvas.java
@@ -9,6 +9,8 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Point2D;
 import java.util.ArrayList;
 
 import javax.swing.JPanel;
@@ -24,55 +26,50 @@ public class Canvas extends JPanel {
 	private Map map;
 	private MapDrawer mapDrawer = new MapDrawer();
 	private ArrayList<Player> players;
-	private double scale = 1;
-	private double x = 0;
-	private double y = 0;
-	private int mouseX;
-	private int mouseY;
 	private Vector2D mousePos;
 	private Station hoveredStation;
 	private Runnable stationClickedRunnable;
+	private AffineTransform at = new AffineTransform();
 
 	public Canvas() {
-		addMouseWheelListener(new MouseWheelListener() {
-			private int scaleSteps = 0;
-			private double lastScale = 1;
-
-			@Override
-			public void mouseWheelMoved(MouseWheelEvent e) {
-				scaleSteps -= e.getWheelRotation();
-				if (scaleSteps > 10)
-					scaleSteps = 10;
-				if (scaleSteps < 0)
-					scaleSteps = 0;
-				scale = 1 + 0.2 * scaleSteps;
-				x = (int) ((mouseX - x / scale) * ((lastScale / scale) - 1) + (lastScale / scale) * x);
-				y = (int) ((mouseY - y / scale) * ((lastScale / scale) - 1) + (lastScale / scale) * y);
-				lastScale = scale;
-				repaint();
-			}
-		});
-		addMouseMotionListener(new MouseMotionListener() {
+		MouseAdapter mouseAdapter = new MouseAdapter() {
+			private double currentX;
+			private double currentY;
+			private double previousX;
+			private double previousY;
+			private double mouseX;
+			private double mouseY;
+			private double zoom = 1;
 
 			@Override
 			public void mouseMoved(MouseEvent e) {
 				mouseX = e.getX();
 				mouseY = e.getY();
+				calculateMousePos();
 				calculateHoveredStation();
 				repaint();
 			}
 
 			@Override
 			public void mouseDragged(MouseEvent e) {
-				x += (e.getX() - mouseX) / scale;
-				y += (e.getY() - mouseY) / scale;
+				currentX += (e.getX() - mouseX) / zoom;
+				currentY += (e.getY() - mouseY) / zoom;
 				mouseX = e.getX();
 				mouseY = e.getY();
+				calculateCurrentTransform();
+				calculateMousePos();
 				calculateHoveredStation();
 				repaint();
 			}
-		});
-		addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseWheelMoved(MouseWheelEvent e) {
+				if (e.getScrollType() == MouseWheelEvent.WHEEL_UNIT_SCROLL) {
+					zoom = Math.min(10, Math.max(0.7, zoom + 0.2 * zoom * -e.getWheelRotation()));
+				}
+				calculateCurrentTransform();
+				repaint();
+			}
 
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -80,20 +77,48 @@ public class Canvas extends JPanel {
 					stationClickedRunnable.run();
 				}
 			}
-		});
+
+			private void calculateCurrentTransform() {
+				at.translate(-previousX, -previousY);
+				Point2D p = getTranslatedPoint(mouseX, mouseY);
+				at.setToIdentity();
+				at.translate(mouseX, mouseY);
+				at.scale(zoom, zoom);
+				at.translate(-p.getX(), -p.getY());
+				at.translate(currentX, currentY);
+				previousX = currentX;
+				previousY = currentY;
+			}
+
+			private void calculateMousePos() {
+				Point2D mouse = getTranslatedPoint(mouseX, mouseY);
+				mousePos = new Vector2D(mouse.getX() / getWidth(), mouse.getY() / getHeight());
+			}
+
+			private Point2D getTranslatedPoint(double panelX, double panelY) {
+				Point2D point2d = new Point2D.Double(panelX, panelY);
+				try {
+					return at.inverseTransform(point2d, null);
+				} catch (Exception e) {
+					e.printStackTrace();
+					return null;
+				}
+
+			}
+
+		};
+		addMouseListener(mouseAdapter);
+		addMouseMotionListener(mouseAdapter);
+		addMouseWheelListener(mouseAdapter);
 	}
 
 	@Override
 	protected void paintComponent(Graphics gg) {
-		mousePos = new Vector2D((mouseX / scale - x) / getWidth(), (mouseY / scale - y) / getHeight());
 		Graphics2D g = (Graphics2D) gg;
 		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
 		g.clearRect(0, 0, getWidth(), getHeight());
-
-		g.scale(scale, scale);
-		g.translate(x, y);
-
+		g.transform(at);
 		g.setColor(new Color(200, 200, 200));
 		g.fillRect(0, 0, getWidth(), getHeight());
 
